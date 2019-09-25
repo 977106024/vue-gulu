640de48f395e135f23586d41568388282dfa53cd
bohong <977106024@qq.com>
2019-09-25 18:54:33 +0800

    完成button基本样式

--- src/App.vue
+++ src/App.vue
@@ -1,39 +1,47 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-<!--    <HelloWorld msg="Welcome to Your Vue.js App"/>-->
-    <g-button>按钮</g-button>
-  </div>
+    <div id="app">
+        <img alt="Vue logo" src="./assets/logo.png">
+        <!--    <HelloWorld msg="Welcome to Your Vue.js App"/>-->
+        <g-button>按钮</g-button>
+    </div>
 </template>
 
 <script>
-// import HelloWorld from './components/HelloWorld.vue'
-// import GButton from './components/Button'
+    // import HelloWorld from './components/HelloWorld.vue'
+    // import GButton from './components/Button'
 
-export default {
-  name: 'app',
-  components: {
-    // GButton
-  }
-}
+    export default {
+        name: 'app',
+        components: {
+            // GButton
+        }
+    }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-  *{
-    padding: 0;
-    margin: 0;
-    box-sizing: border-box;
-  }
-  :root{
-    --button-height:32px;
-    --font-size:14px;
-  }
+    #app {
+        font-family: 'Avenir', Helvetica, Arial, sans-serif;
+        -webkit-font-smoothing: antialiased;
+        -moz-osx-font-smoothing: grayscale;
+        text-align: center;
+        color: #2c3e50;
+        margin-top: 60px;
+    }
+
+    * {
+        padding: 0;
+        margin: 0;
+        box-sizing: border-box;
+    }
+
+    :root {
+        --button-height: 32px;
+        --font-size: 14px;
+        --button-bg: white;
+        --button-active-bg: #eee;
+        --border-radius: 4px;
+        --color: #999;
+        --border-color: #999;
+        --border-color-hover: #666;
+    }
 </style>
