447911b5bc0876b3649e80f11cbbed99f681d561
bohong <977106024@qq.com>
2019-09-29 17:56:30 +0800

    单元测试->添加chaijs

--- src/main.js
+++ src/main.js
@@ -11,9 +11,25 @@ Vue.component('g-icon',GIcon)
 Vue.component('button-group', ButtonGroup)
 
 
-
 Vue.config.productionTip = false
 
 new Vue({
   render: h => h(App),
 }).$mount('#app')
+
+//单元测试
+import chai from 'chai'
+const expect = chai.expect
+{
+  const Constructor = Vue.extend(GButton)
+  const button = new Constructor({
+    propsData:{
+      icon:'set'
+    }
+  })
+  button.$mount()
+
+  let svg = button.$el.querySelector('use')
+  let svgHref = svg.getAttribute('xlink:href')
+  expect(svgHref).to.eq('#i-set')
+}
