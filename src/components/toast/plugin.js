0a6a6071e12aaec486deeec6feb1ce92ab4ed673
bohong <977106024@qq.com>
2019-10-16 23:08:27 +0800

    实现只能存在一个toast的功能

--- src/components/toast/plugin.js
+++ src/components/toast/plugin.js
@@ -1,9 +1,14 @@
 import toast from './Toast'
+
+let currentToast
 //vue插件写法
 export default{
     install(Vue,options){
         Vue.prototype.$toast = function(message,toastOptions){
-            createdToast({Vue,message,propsData:toastOptions})
+            if(currentToast){
+                currentToast.close() //存在toast实列 就调用toast本身的销毁函数
+            }
+            currentToast = createdToast({Vue,message,propsData:toastOptions})
         }
     }
 }
@@ -16,4 +21,6 @@ function createdToast({Vue,message,propsData}){
     vm.$slots.default = message //文字
     vm.$mount() //挂载到内存中
     document.body.appendChild(vm.$el) //挂到页面上
+    console.log('what？',vm)
+    return vm
 }
