d2cf5c58116d0e44aa53e61013025ec0cc29393d
bohong <977106024@qq.com>
2019-10-17 14:43:38 +0800

    解决currentToast 重复close()的bug

--- src/components/toast/plugin.js
+++ src/components/toast/plugin.js
@@ -8,19 +8,28 @@ export default{
             if(currentToast){
                 currentToast.close() //存在toast实列 就调用toast本身的销毁函数
             }
-            currentToast = createdToast({Vue,message,propsData:toastOptions})
+            currentToast = createdToast({
+                Vue,
+                message,
+                propsData:toastOptions,
+                onClose:()=>{ //点击关闭时 不需要重复调用currentToast.close()
+                    currentToast = null
+                }
+            })
         }
     }
 }
 
-function createdToast({Vue,message,propsData}){
+
+
+function createdToast({Vue,message,propsData,onClose}){
     let Constructor = Vue.extend(toast)
     let vm = new Constructor({ //创建toast
         propsData //props
     })
     vm.$slots.default = message //文字
     vm.$mount() //挂载到内存中
+    vm.$on('close',onClose) //监听toast的$emit
     document.body.appendChild(vm.$el) //挂到页面上
-    console.log('what？',vm)
     return vm
 }
