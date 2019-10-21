42b6ad2b665f71974026477f7a26e966f9ab967d
bohong <977106024@qq.com>
2019-10-21 16:35:31 +0800

    重构tabs代码

--- src/components/tabs/Tabs.vue
+++ src/components/tabs/Tabs.vue
@@ -31,19 +31,27 @@
             }
         },
         mounted() {
-            if(this.$children.length === 0){
-                console && console.warn &&
-                console.warn('tabs子组件应该是tabs-head和tabs-body,但你没写子组件。')
-            }
-            this.$children.forEach(vm=>{
-                if(vm.$options.name === 'guluTabsHead'){ //tabs的子元素是head
-                    vm.$children.forEach(itemVm=>{
-                        if(itemVm.$options.name === 'guluTabsItem' && itemVm.name === this.selected){ //head的子元素是item 并且name是选中的这个
-                            this.eventBus.$emit('update:selected',this.selected,itemVm) //通知事件中心 用户默认选中
-                        }
-                    })
+            this.checkChildren()
+            this.selectTab()
+        },
+        methods:{
+            checkChildren(){
+                if(this.$children.length === 0){
+                    console && console.warn &&
+                    console.warn('tabs子组件应该是tabs-head和tabs-body,但你没写子组件。')
                 }
-            })
+            },
+            selectTab(){
+                this.$children.forEach(vm=>{
+                    if(vm.$options.name === 'guluTabsHead'){ //tabs的子元素是head
+                        vm.$children.forEach(itemVm=>{
+                            if(itemVm.$options.name === 'guluTabsItem' && itemVm.name === this.selected){ //head的子元素是item 并且name是选中的这个
+                                this.eventBus.$emit('update:selected',this.selected,itemVm) //通知事件中心 用户默认选中
+                            }
+                        })
+                    }
+                })
+            }
         }
 
     }
