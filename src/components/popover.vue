50049852ec7797991b60340f29a9d34efc92da72
bohong <977106024@qq.com>
2019-10-21 16:32:09 +0800

    重构popover代码

--- src/components/popover.vue
+++ src/components/popover.vue
@@ -16,35 +16,38 @@
             visible:false
         }),
         methods:{
+            positionContent(){
+                document.body.appendChild(this.$refs.contentWrap) //放到body中 不然会被父元素overflow：hidden遮挡
+                //内容定位在按钮上方
+                let {top,left } = this.$refs.trigeerWrap.getBoundingClientRect()           //----------->这里获取的距离是视窗距离 如果有页面较长 有滚动部分就会定位偏差
+                this.$refs.contentWrap.style.top = `${top + window.scrollY}px`  //可视窗口 + 滚动距离 因为trigeer(按钮)相对于视窗 而content(弹出的内容)是相对于document定位。(document包含超出视窗的滚动条距离)
+                this.$refs.contentWrap.style.left = `${left + window.scrollX}px`
+            },
+            onClickDocument (e){ //原本用bind绑定this 但是bind会生成一个新函数
+                if(!this.$refs.contentWrap.contains(e.target)){ //点击content 就不会触发document的click
+                    this.close()
+                }
+            },
+            open(){ //打开时 先定位 然后绑定document事件 等下次点击时可以冒泡关闭 但是点击content不会触发
+                this.visible = true
+                // this.$nextTick(function(){
+                setTimeout(()=>{ //延时绑定时间 不然会一起触发click
+                    this.positionContent()
+                    document.addEventListener('click',this.onClickDocument)//绑定事件
+                })
+                // })
+            },
+            close(){ //关闭时 关闭和移出document上的点击事件
+                this.visible = false
+                document.removeEventListener('click',this.onClickDocument)//点击后删除事件
+            },
             onClick(event){
                 if(this.$refs.trigeerWrap.contains(event.target)){ //是否点击的是trigeer里面的元素(按钮)
-                    console.log('里面')
-                    this.visible = !this.visible
-                    //关闭popover
-                    if(this.visible){
-                        // this.$nextTick(function(){
-                        setTimeout(()=>{ //延时绑定时间 不然会一起触发click
-                            document.body.appendChild(this.$refs.contentWrap) //放到body中 不然会被父元素overflow：hidden遮挡
-
-                            //内容定位在按钮上方
-                            let {top,left } = this.$refs.trigeerWrap.getBoundingClientRect()           //----------->这里获取的距离是视窗距离 如果有页面较长 有滚动部分就会定位偏差
-                            this.$refs.contentWrap.style.top = `${top + window.scrollY}px`  //可视窗口 + 滚动距离 因为trigeer(按钮)相对于视窗 而content(弹出的内容)是相对于document定位。(document包含超出视窗的滚动条距离)
-                            this.$refs.contentWrap.style.left = `${left + window.scrollX}px`
-
-                            let eventHandler = (e)=>{ //原本用bind绑定this 但是bind会生成一个新函数
-                                console.log(e.target)
-                                if(!this.$refs.contentWrap.contains(e.target)){ //不是点击content 就不会触发document的click
-                                    this.visible = false
-                                    document.removeEventListener('click',eventHandler)//点击后删除事件
-                                    console.log('关闭？')
-                                }
-                            }
-                            document.addEventListener('click',eventHandler)//绑定事件
-                        })
-                        // })
+                    if(!this.visible){
+                        this.open()
+                    }else{
+                        this.close()
                     }
-                }else{
-                    console.log('外面')
                 }
             }
         }
